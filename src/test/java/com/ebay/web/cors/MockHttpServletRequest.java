613dd2a41c2f4b310c47b81e846670848587ca34
Mohit Soni <mosoni@ebay.com>
2013-05-08 20:51:25 -0700

    Cleanup.

--- src/test/java/com/ebay/web/cors/MockHttpServletRequest.java
+++ src/test/java/com/ebay/web/cors/MockHttpServletRequest.java
@@ -35,83 +35,82 @@ public class MockHttpServletRequest implements HttpServletRequest {
     }
 
     public String getCharacterEncoding() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public void setCharacterEncoding(String env)
             throws UnsupportedEncodingException {
-        // TODO Auto-generated method stub
 
     }
 
     public int getContentLength() {
-        // TODO Auto-generated method stub
+
         return 0;
     }
 
     public String getContentType() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public ServletInputStream getInputStream() throws IOException {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getParameter(String name) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public Enumeration getParameterNames() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String[] getParameterValues(String name) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public Map getParameterMap() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getProtocol() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getScheme() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getServerName() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public int getServerPort() {
-        // TODO Auto-generated method stub
+
         return 0;
     }
 
     public BufferedReader getReader() throws IOException {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getRemoteAddr() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getRemoteHost() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
@@ -124,62 +123,62 @@ public class MockHttpServletRequest implements HttpServletRequest {
     }
 
     public Locale getLocale() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public Enumeration getLocales() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public boolean isSecure() {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
     public RequestDispatcher getRequestDispatcher(String path) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getRealPath(String path) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public int getRemotePort() {
-        // TODO Auto-generated method stub
+
         return 0;
     }
 
     public String getLocalName() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getLocalAddr() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public int getLocalPort() {
-        // TODO Auto-generated method stub
+
         return 0;
     }
 
     public String getAuthType() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public Cookie[] getCookies() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public long getDateHeader(String name) {
-        // TODO Auto-generated method stub
+
         return 0;
     }
 
@@ -199,7 +198,7 @@ public class MockHttpServletRequest implements HttpServletRequest {
     }
 
     public Enumeration getHeaders(String name) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
@@ -208,7 +207,7 @@ public class MockHttpServletRequest implements HttpServletRequest {
     }
 
     public int getIntHeader(String name) {
-        // TODO Auto-generated method stub
+
         return 0;
     }
 
@@ -221,87 +220,87 @@ public class MockHttpServletRequest implements HttpServletRequest {
     }
 
     public String getPathInfo() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getPathTranslated() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getContextPath() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getQueryString() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getRemoteUser() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public boolean isUserInRole(String role) {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
     public Principal getUserPrincipal() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getRequestedSessionId() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getRequestURI() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public StringBuffer getRequestURL() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getServletPath() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public HttpSession getSession(boolean create) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public HttpSession getSession() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public boolean isRequestedSessionIdValid() {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
     public boolean isRequestedSessionIdFromCookie() {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
     public boolean isRequestedSessionIdFromURL() {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
     public boolean isRequestedSessionIdFromUrl() {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
--- src/test/java/com/ebay/web/cors/MockHttpServletResponse.java
+++ src/test/java/com/ebay/web/cors/MockHttpServletResponse.java
@@ -15,132 +15,118 @@ public class MockHttpServletResponse implements HttpServletResponse {
     List headerValues = new ArrayList();
 
     public String getCharacterEncoding() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String getContentType() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public ServletOutputStream getOutputStream() throws IOException {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public PrintWriter getWriter() throws IOException {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public void setCharacterEncoding(String charset) {
-        // TODO Auto-generated method stub
 
     }
 
     public void setContentLength(int len) {
-        // TODO Auto-generated method stub
 
     }
 
     public void setContentType(String type) {
-        // TODO Auto-generated method stub
 
     }
 
     public void setBufferSize(int size) {
-        // TODO Auto-generated method stub
 
     }
 
     public int getBufferSize() {
-        // TODO Auto-generated method stub
+
         return 0;
     }
 
     public void flushBuffer() throws IOException {
-        // TODO Auto-generated method stub
 
     }
 
     public void resetBuffer() {
-        // TODO Auto-generated method stub
 
     }
 
     public boolean isCommitted() {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
     public void reset() {
-        // TODO Auto-generated method stub
 
     }
 
     public void setLocale(Locale loc) {
-        // TODO Auto-generated method stub
 
     }
 
     public Locale getLocale() {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public void addCookie(Cookie cookie) {
-        // TODO Auto-generated method stub
 
     }
 
     public boolean containsHeader(String name) {
-        // TODO Auto-generated method stub
+
         return false;
     }
 
     public String encodeURL(String url) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String encodeRedirectURL(String url) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String encodeUrl(String url) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public String encodeRedirectUrl(String url) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     public void sendError(int sc, String msg) throws IOException {
-        // TODO Auto-generated method stub
 
     }
 
     public void sendError(int sc) throws IOException {
-        // TODO Auto-generated method stub
 
     }
 
     public void sendRedirect(String location) throws IOException {
-        // TODO Auto-generated method stub
 
     }
 
     public void setDateHeader(String name, long date) {
-        // TODO Auto-generated method stub
 
     }
 
     public void addDateHeader(String name, long date) {
-        // TODO Auto-generated method stub
 
     }
 
@@ -168,22 +154,18 @@ public class MockHttpServletResponse implements HttpServletResponse {
     }
 
     public void setIntHeader(String name, int value) {
-        // TODO Auto-generated method stub
 
     }
 
     public void addIntHeader(String name, int value) {
-        // TODO Auto-generated method stub
 
     }
 
     public void setStatus(int sc) {
-        // TODO Auto-generated method stub
 
     }
 
     public void setStatus(int sc, String sm) {
-        // TODO Auto-generated method stub
 
     }
 
